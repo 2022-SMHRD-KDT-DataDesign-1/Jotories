d80b65d50315a262857e84936b39feee8da466d6
youngjuj <alti100@likelion.org>
2023-03-30 17:38:26 +0900

    commit

--- src/main/java/com/dogdog/controller/ReservationService.java
+++ src/main/java/com/dogdog/controller/ReservationService.java
@@ -22,14 +22,15 @@ public class ReservationService implements Service{
 		String user_id = (String)session.getAttribute("user_id");
 		int dog_no = Integer.parseInt(request.getParameter("dog_no"));
 		// 예약 시작일
-		String member_from = (String)request.getParameter("member_from");
+		String member_from = request.getParameter("member_from");
 		// 예약 종료일
-		String member_to = (String)request.getParameter("member_to");
+		String member_to = request.getParameter("member_to");
 		// 특이사항 노트
-		String member_note = (String)request.getParameter("member_note");
-		// 예약 등록일
+		String member_note = request.getParameter("member_note");
+		String member_name = request.getParameter("member_name");
+		String user_phone = request.getParameter("user_phone");
 		
-		StoreMemberVO vo = new StoreMemberVO(store_id, user_id, dog_no, member_from, member_to, member_note);
+		StoreMemberVO vo = new StoreMemberVO(store_id, user_id, dog_no, member_from, member_to, member_note, member_name, user_phone);
 		StoreMemberDAO dao = new StoreMemberDAO();
 		
 		int cnt = dao.insertMember(vo);
